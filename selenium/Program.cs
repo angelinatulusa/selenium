a837afd5e3b7e8467e972276b9d1e9384237f757
opilane <opilane@HP-CZC1339D2P>
2023-10-02 13:59:18 +0300

    Add project files.

---
+++ selenium/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+class Program
+{
+    static void Main()
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("--disable-notifications");
+
+        IWebDriver driver = new ChromeDriver(options);
+
+        driver.Navigate().GoToUrl("https://tulusa21.thkit.ee/?leht=kukkumine.php");
+
+        var buttons = driver.FindElements(By.Id("btn"));
+        bool allbuttons = true;
+
+        foreach (var button in buttons)
+        {
+            if (button.Displayed)
+            {
+                System.Threading.Thread.Sleep(1000);
+                button.Click();
+                IWebElement successMessage = null;
+                try
+                {
+                    successMessage = driver.FindElement(By.Id("success-message"));
+                }
+                catch (NoSuchElementException)
+                {
+                    allbuttons = false;
+                }
+
+                if (successMessage != null && successMessage.Displayed)
+                {
+                }
+                else
+                {
+                    allbuttons = false;
+                }
+            }
+        }
+        if (allbuttons)
+        {
+            Console.WriteLine("All buttons work!");
+        }
+        else
+        {
+            Console.WriteLine("Something went wrong");
+        }
+        System.Threading.Thread.Sleep(1000);
+        driver.Quit();
+    }
+}
